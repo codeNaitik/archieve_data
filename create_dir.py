9c85f37113adb1e3c2a6eb60861c28d6715d14ac
KoushikTeja <koushik.viluvala@gmail.com>
2024-03-17 15:52:55 +0530

    29 completed 24 corrected and 5 discarded

--- create_dir.py
+++ create_dir.py
@@ -2,14 +2,14 @@ import os
 import subprocess
 
 def create_directory_with_files(directory_name):
-    # Create the directory
+
     os.makedirs(directory_name, exist_ok=True)
 
-    # Define file names
+
     file1_name = os.path.join(directory_name, f"{directory_name}.txt")
     file2_name = os.path.join(directory_name, f"{directory_name}_cleaned.txt")
 
-    # Create the files
+
     with open(file1_name, 'w') as file1:
         file1.write("")
 
@@ -19,8 +19,8 @@ def create_directory_with_files(directory_name):
     print(f"Directory '{directory_name}' created with files '{file1_name}' and '{file2_name}'.")
 
 
-# Get the directory name from the user
-directory_name = "in.ernet.dli.2015.343839"
 
-# Call the function to create the directory and files
+directory_name = "in.ernet.dli.2015.269041"
+
+
 create_directory_with_files(directory_name)
